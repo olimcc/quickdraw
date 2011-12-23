cf3e44dc3990368bde66f70b320a4e84115b8b38
Oli Mcc <dev@olimcc.com>
2011-12-23 10:38:59 -0800

    clean up - break out setUp/Teardown

--- main.js
+++ main.js
@@ -23,33 +23,34 @@ rappad.Drawer = function(divId, width, height, opts) {
 
     // after the mousedown happens
     this.setUp = function(e) {
-        return this.paper.path([["M", e.offsetX, e.offsetY]]);
+        this.currentPath = this.paper.path([["M", e.offsetX, e.offsetY]]);
+        this.currentPath.mouseup(function () {
+            pad.tearDown();
+        });
+    }
+
+    this.updatePath = function(x, y) {
+          var arr = this.currentPath.attrs.path;
+          arr.push(["L", x, y]);
+          this.currentPath.attr({path: arr});
+          return;
     }
 
     // attach handlers for rect
     // when a mouse down happens
     this.rect.mousedown(function () {
         pad.mouseIsDown = true;
-        return false;
     });
 
     // when a mousemove happens
     this.rect.mousemove(function (e) {
         if (pad.mouseIsDown) {
           if (!pad.currentPath) {
-            // create the current path if it doesn't exist
-                pad.currentPath = pad.setUp(e);
-              // attach a click handler to the path
-              pad.currentPath.mouseup(function () {
-                pad.tearDown();
-              });
+            pad.setUp(e);
+          } else {
+            pad.updatePath(e.offsetX, e.offsetY);
           }
-          var arr = pad.currentPath.attrs.path;
-          arr.push(["L", e.offsetX, e.offsetY]);
-          // this appears to return p to the handler
-          pad.currentPath.attr({path: arr});
         }
-        return false;
     });
 
     // attach mouseup handlers to end line draw
@@ -57,7 +58,6 @@ rappad.Drawer = function(divId, width, height, opts) {
         if (pad.mouseIsDown) {
             pad.tearDown();
         }
-        return false;
     });
 
 }
