dd99585390891b403a453d1aecece53d1aa66550
Oli Mcc <dev@olimcc.com>
2011-12-23 11:28:28 -0800

    break out new paths into rappad.Line class

--- main.js
+++ main.js
@@ -1,56 +1,67 @@
 var rappad = {};
 
+rappad.Line = function(drawer, x, y, opt_id) {
+    this.id = opt_id || null;
+    this.path = drawer.paper.path([["M", x, y]]);
+    this.path.mouseup(function () {
+        drawer.tearDown();
+    });
+    bla = this;
+}
+
+rappad.Line.prototype.updatePath = function(x, y) {
+    var arr = this.path.attrs.path;
+    arr.push(["L", x, y]);
+    this.path.attr({path: arr});
+    return;
+}
+
+rappad.Line.prototype.setPath = function(pathStr) {
+    this.path.attr({path: pathStr});
+    return;
+}
+
+rappad.Line.prototype.getPath = function() {
+    return this.path.attrs.path;
+}
+
 rappad.Drawer = function(divId, width, height, opts) {
     this.paper = Raphael(divId, width, height);
     this.mouseIsDown = false;
-    this.currentPath = null;    
+    this.activeLine = null;    
     this.rect = this.paper.rect(0, 0, this.paper.width, this.paper.height);
     this.rect.attr({
         'fill': 'white',
         'stroke': 'white',
         'fill-opacity': 0,
         'stroke-opacity': 0});
-    this.pathSet = this.paper.set();
+    this.localLines = [];
     var pad = this;
 
     // after the mouseup happens
     this.tearDown = function() {
         this.mouseIsDown = false;
-        this.pathSet.push(this.currentPath);
-        this.currentPath = null;
+        this.localLines.push(this.activeLine);
         return;
     }
 
     // after the mousedown happens
-    this.setUp = function(e) {
-        this.currentPath = this.paper.path([["M", e.offsetX, e.offsetY]]);
-        this.currentPath.mouseup(function () {
-            pad.tearDown();
-        });
-    }
-
-    this.updatePath = function(x, y) {
-          var arr = this.currentPath.attrs.path;
-          arr.push(["L", x, y]);
-          this.currentPath.attr({path: arr});
-          return;
+    this.setUp = function(x, y) {
+        this.activeLine = new rappad.Line(this, x, y);
     }
 
     // attach handlers for rect
     // when a mouse down happens
-    this.rect.mousedown(function () {
+    this.rect.mousedown(function (e) {
         pad.mouseIsDown = true;
+        pad.setUp(e.offsetX, e.offsetY);
     });
 
     // when a mousemove happens
     this.rect.mousemove(function (e) {
         if (pad.mouseIsDown) {
-          if (!pad.currentPath) {
-            pad.setUp(e);
-          } else {
-            pad.updatePath(e.offsetX, e.offsetY);
+            pad.activeLine.updatePath(e.offsetX, e.offsetY);
           }
-        }
     });
 
     // attach mouseup handlers to end line draw
