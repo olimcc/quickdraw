7ad40dce6eaa3132ad375a2cd5a1ba0c871fcf24
Oli Mcc <dev@olimcc.com>
2011-12-29 01:02:11 -0800

    adding favicon.ico

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ exports.configure = function() {
 
     this.port = 1337;
     this.testPadActivityInterval = 1000 * 60 * 5; // 5 mins
-    this.padTimeoutTolerance = 1000 * 60 * 15; // 15 mins
+    this.padTimeoutTolerance = 100 * 60 * 15; // 15 mins
     return this;
 };
