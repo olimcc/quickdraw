af4ede2c424bdaa05de2c303d5081fbc13e4bb7a
Oli Mcc <dev@olimcc.com>
2011-12-30 16:21:29 -0800

    cleanup FE code

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,32 @@
 var quickdraw = {};
 quickdraw.utils = quickdraw.utils || {};
 
+quickdraw.utils.getDivPosition = function(divId) {
+    var d = document.getElementById(divId);
+    return {x: d.offsetLeft, y: d.offsetTop};
+}
+
+quickdraw.utils.getCoordsFromMouseEvent = function(event, divId) {
+    var pos = quickdraw.utils.getDivPosition(divId);
+    var x = event.offsetX ? event.offsetX : event.clientX - pos.x;
+    var y = event.offsetY ? event.offsetY : event.clientY - pos.y;
+    return [x, y];
+}
+
+/**
+ * Handle incoming and outgoing messages to a pad
+ * @param {quickpad.Drawer} drawer instance
+ * @param {String} pid Pad Id
+ */
 quickdraw.Messenger = function(drawer, pid) {
     this._drawer = drawer;
     this._pid = pid;
 }
+
+/**
+ * Handle an incoming message. Right now, only writes paths.
+ * @param {String} msg Path string
+ */
 quickdraw.Messenger.prototype.handleMessageIn = function(msg) {
     var p = new quickdraw.Line(this._drawer, {});
     p.setPath(msg.path);
@@ -12,6 +34,12 @@ quickdraw.Messenger.prototype.handleMessageIn = function(msg) {
     return;
 }
 
+/**
+ * Handle an outgoing message. Triggers AJAX request.
+ * @param {String} client id
+ * @param {String} method ('update', 'delete')
+ * @param {String} path to send
+ */
 quickdraw.Messenger.prototype.handleMessageOut = function(client, method, path) {
     $.ajax({
         dataType: 'text',
@@ -27,18 +55,16 @@ quickdraw.Messenger.prototype.handleMessageOut = function(client, method, path) 
     });
 }
 
-quickdraw.utils.getDivPosition = function(divId) {
-    var d = document.getElementById(divId);
-    return {x: d.offsetLeft, y: d.offsetTop};
-}
-
-quickdraw.utils.getCoordsFromMouseEvent = function(event, divId) {
-    var pos = quickdraw.utils.getDivPosition(divId);
-    var x = event.offsetX ? event.offsetX : event.clientX - pos.x;
-    var y = event.offsetY ? event.offsetY : event.clientY - pos.y;
-    return [x, y];
-}
-
+/**
+ * Wrapper around standard raphael path, proving extra functionality.
+ *
+ * If opts.x and opts.y exists, inits a path at that point, else, creates empty
+ * path.
+ * Attaches a mouseup event to the path drawing.
+ *
+ * @param {quickdraw.Drawer} drawer instance
+ * @param {Object} opts dict
+ */
 quickdraw.Line = function(drawer, opts) {
     this.id = opts.id || null;
     if (opts.x && opts.y) {
@@ -51,6 +77,11 @@ quickdraw.Line = function(drawer, opts) {
     });
 }
 
+/**
+ * Append values to the path.
+ * @param {Number} x coord
+ * @param {Number} y coord
+ */
 quickdraw.Line.prototype.updatePath = function(x, y) {
     var arr = this.path.attrs.path;
     if (typeof(arr) == 'string') {
@@ -62,15 +93,37 @@ quickdraw.Line.prototype.updatePath = function(x, y) {
     return;
 }
 
+/**
+ * Completely update the path
+ * @param {String} Complete path string
+ */
 quickdraw.Line.prototype.setPath = function(pathStr) {
     this.path.attr({path: pathStr});
     return;
 }
 
+/**
+ * Get a String representation of the path.
+ */
 quickdraw.Line.prototype.getPath = function() {
     return this.path.attrs.path;
 }
 
+/**
+ * Our main drawing pad.
+ *
+ * Handles rendering Raphael paper object for drawing, captures click/move events
+ * on the area, triggers creation of quickdraw.Line objects on the pad.
+ *
+ * In order to capture mousedown events on the paper area, an invisible rectangle
+ * is created matching the same dimensions as the paper (see this.rect).
+ * Mouse/Touch events are captured here.
+ *
+ * @param {String} divId to place pad
+ * @param {String} width
+ * @param {String} height
+ * @param {Object} opts dict
+ */
 quickdraw.Drawer = function(divId, width, height, opts) {
     this.divId = divId;
     this.paper = Raphael(divId, width, height);
@@ -89,6 +142,10 @@ quickdraw.Drawer = function(divId, width, height, opts) {
 
     // after the mouseup happens
 
+    /**
+     * Called when a mouseup event happens.
+     * saves line, disables this.mouseIsDown.
+     */
     this.tearDown = function() {
         this.mouseIsDown = false;
         this.setLineToNamespace(this.activeLine);
@@ -98,26 +155,36 @@ quickdraw.Drawer = function(divId, width, height, opts) {
         return;
     }
 
-    // after the mousedown happens
+    /**
+     * Sets the active line
+     * @param {Number} x coord to start at
+     * @param {Number} y coord to start at
+     */
     this.newLine = function(x, y) {
         this.activeLine = new quickdraw.Line(this, {x: x, y: y});
     }
 
+    /**
+     * Stores a quickdraw.Line object to a given pad namespace.
+     * A namespace may represent the originating location of a line
+     * (for example, the id of the person that created it.)
+     *
+     * @param {quickdraw.Line} line object
+     * @param {String} ns namespace name
+     */
     this.setLineToNamespace = function(line, ns) {
         var ns = ns || 'local';
         if (!this.lines[ns]) this.lines[ns] = [];
         this.lines[ns].push(line);
     }
 
-    // attach handlers for rect
-    // when a mouse down happens
+    // attach mouse handlers for rect
     this.rect.mousedown(function (e) {
         pad.mouseIsDown = true;
         co = quickdraw.utils.getCoordsFromMouseEvent(e, pad.divId);
         pad.newLine(co[0], co[1]);
     });
 
-    // when a mousemove happens
     this.rect.mousemove(function (e) {
         if (pad.mouseIsDown) {
             co = quickdraw.utils.getCoordsFromMouseEvent(e, pad.divId);
@@ -125,15 +192,13 @@ quickdraw.Drawer = function(divId, width, height, opts) {
           }
     });
 
-    // attach mouseup handlers to end line draw
     this.rect.mouseup(function () {
         if (pad.mouseIsDown) {
             pad.tearDown();
         }
     });
 
-    // attach handlers for rect
-    // when a mouse down happens
+    // attach touch handlers for rect
     this.rect.touchstart(function (e) {
         pad.mouseIsDown = true;
         e = e.changedTouches[0];
@@ -141,7 +206,6 @@ quickdraw.Drawer = function(divId, width, height, opts) {
         pad.newLine(co[0], co[1]);
     });
 
-    // when a mousemove happens
     this.rect.touchmove(function (e) {
         if (pad.mouseIsDown) {
             e = e.changedTouches[0];
@@ -150,7 +214,6 @@ quickdraw.Drawer = function(divId, width, height, opts) {
           }
     });
 
-    // attach mouseup handlers to end line draw
     this.rect.touchend(function () {
         e = e.changedTouches[0];
         if (pad.mouseIsDown) {
