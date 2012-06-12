92256aed947b03e905f4b5ef15ad8673765bd232
Oli Mcc <dev@olimcc.com>
2012-06-11 21:58:16 -0700

    Force websockets to communicate on port 3000.
    Workaround for apache config difficulty.

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 exports.configure = function() {
 
-    this.port = 80;
+    this.port = 3000;
     this.testPadActivityInterval = 1000 * 60 * 5; // 5 mins
     this.padTimeoutTolerance = 1000 * 60 * 15; // 15 mins
     return this;
